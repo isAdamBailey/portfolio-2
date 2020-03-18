dcf3c2db56f402b17f7850e626413af93b7bec52
Adam Bailey <bailey@hyphenfi.com>
2020-03-18 08:06:59 -0700

    Initial project setup

---
+++ README.md
@@ -0,0 +1,16 @@
+# Portfolio for Adam Bailey
+
+## Credits
+
+* This portfolio was based on the starter blog initially created by [Joseph Dyer](https://github.com/josephdyer).
+
+
+## Features
+
+* Gridsome
+* GraphQL
+* Tailwindcss
+* SCSS
+* Markdown
+* Tags
+* Blog
---
+++ src/pages/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <Layout>
+    <section id="container-centre" class="column centre flex-1">
+      <h1 class="page-title">Hi! I'm Adam Bailey</h1>
+      <div class="content">
+        <p>
+          I'm an experienced Software Developer working primarily in
+          <a href="https://laravel.com/">Laravel</a> and
+          <a href="https://vuejs.org/">Vue.js</a>.
+        </p>
+        <h2 class="text-3xl">Features</h2>
+        <ul class="mb-12 list-disc pl-4">
+          <li>Build sites faster with the power of Gridsome, TailwindCSS and SCSS</li>
+          <li>Image lazyloading via gridsome builtin functionality</li>
+          <li>A simple blog with categories, tags and featured images</li>
+        </ul>
+      </div>
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Home"
+  }
+};
+</script>
