fbad511b75cfdc889d2e212dc25d05f727b5d39e
Adam Bailey <bailey@hyphenfi.com>
2020-03-20 14:57:04 -0700

    New card style for projects

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -20,16 +20,16 @@
         <div class="page-title mt-12">Projects</div>
         <p class="text-gray-700">While most of my best work was done in private repositories, Here are some examples
           of open source projects ive worked on.</p>
-        <div
-          v-for="project in $static.metadata.projects"
-          class="max-w-sm w-full lg:max-w-full lg:flex mb-4 shadow-lg"
-        >
-          <div class="border-r border-b border-l border-t border-gray-300 bg-white rounded p-4 flex flex-col justify-between">
-            <div>
+        <div class="posts flex justify-center flex-wrap">
+          <div
+            v-for="project in $static.metadata.projects"
+            class="flex flex-col justify-between max-w-md rounded overflow-hidden shadow-lg m-4"
+          >
+            <div class="p-4">
               <div class="text-gray-900 font-bold text-3xl">{{ project.title }}</div>
               <p class="text-gray-700 text-base">{{ project.description }}</p>
             </div>
-            <div class="flex justify-center inline-flex h-12">
+            <div class="justify-center inline-flex pb-4">
               <a :href="project.siteLink">
                 <button class="rounded-l py-2 px-4 w-40 text-white font-bold bg-pink-500 hover:bg-pink-600">
                   Live Site
