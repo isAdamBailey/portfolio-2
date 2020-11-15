4b07a9a1439433537bdfc6dd993db14adf9a8911
Adam Bailey <bailey@hyphenfi.com>
2020-11-14 16:10:29 -0800

    Move typing animation code into a vue component.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,7 +4,8 @@
       <h1 class="page-title">Hi! I'm Adam Bailey</h1>
       <div class="content">
         <p>
-          I'm a Software Developer experienced in <span id="swap-text"></span>
+          I'm a Software Developer experienced in
+          <TypingText :words="['Laravel.', 'PHP.', 'Javascript.', 'Vue.']"/>
         </p>
 
         <div class="flex justify-center">
@@ -42,94 +43,13 @@
 </template>
 
 <script>
-class SwappableHeading {
-  constructor(element, headings = []) {
-    this.element = element;
-    this.headings = headings;
-    this.current = 1;
-  }
-
-  async swap() {
-    while (true) {
-      await this.wait(1000);
-
-      await this.clear();
-
-      await this.type(this.nextHeading());
-    }
-  }
-
-  async type(text) {
-    while (text.length) {
-      await this.append(text[0]);
-
-      text = text.substring(1);
-    }
-  }
-
-  text() {
-    return this.element.innerHTML;
-  }
-
-  append(text) {
-    this.element.innerHTML += text;
-
-    return this.wait(100);
-  }
-
-  async clear() {
-    while (this.length()) {
-      await this.backspace();
-    }
-  }
-
-  backspace() {
-    this.element.innerHTML = this.text().slice(0, -1);
-
-    return this.wait(100);
-  }
-
-  nextHeading() {
-    let heading = this.headings[this.current - 1];
-
-    this.increment();
-
-    return heading;
-  }
-
-  length() {
-    return this.text().length;
-  }
-
-  increment() {
-    this.current++;
-
-    if (this.current > this.headings.length) {
-      this.current = 1;
-    }
-  }
-
-  async wait(milliseconds) {
-    return new Promise(resolve => {
-      setTimeout(resolve, milliseconds);
-    });
-  }
-}
+import TypingText from '../layouts/partials/TypingText'
 
 export default {
+  components: { TypingText },
   metaInfo: {
     title: "Home"
   },
-
-  data() {
-    return {
-      headings: ['Laravel.', 'PHP.', 'Javascript.', 'Vue.']
-    };
-  },
-
-  mounted() {
-    new SwappableHeading(document.getElementById('swap-text'), this.headings).swap();
-  }
 };
 </script>
 
@@ -145,10 +65,3 @@ export default {
     }
   }
 </static-query>
-
-<style lang="scss" scoped>
-#swap-text {
-  font-weight: bold;
-  font-size: 2rem;
-}
-</style>
